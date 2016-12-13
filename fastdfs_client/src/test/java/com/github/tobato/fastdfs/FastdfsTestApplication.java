3acb4227d74b0589f4419740b5d3aff5aef06e74
double-qiu <934590736@qq.com>
2016-12-13 18:54:22 +0800

    init fastdfs_client

---
+++ fastdfs_client/src/test/java/com/github/tobato/fastdfs/FastdfsTestApplication.java
@@ -0,0 +1,18 @@
+package com.github.tobato.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 测试驱动类
+ * 
+ * @author tobato
+ *
+ */
+@SpringBootApplication
+public class FastdfsTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FastdfsTestApplication.class, args);
+    }
+
+}
